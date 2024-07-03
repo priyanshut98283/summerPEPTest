f0efc9f6ae8ce7634c6b0eb62eecd5607ba72414
Priyanshu Tiwari <priyanshut98283@gmail.com>
2024-07-03 10:17:13 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Programiz from './components/programizUI';
+import ToDo from './components/todo';
 
 function App() {
   return (
     <div className="App">
-      <Programiz/>
+      <ToDo/>
     </div>
   );
 }
